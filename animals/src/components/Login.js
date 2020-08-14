b9d54a26c4f91473eebdfa7aed5d0aba3491f19e
Sawyer Pearson <sawyer.pearson92@gmail.com>
2020-08-13 19:11:42 -0600

    login set up and functioning properly, saving auth-token to local storage

--- animals/src/components/Login.js
+++ animals/src/components/Login.js
@@ -1,12 +1,58 @@
-import React from "react";
+import React, {useState} from "react";
+import {axiosWithAuth} from '../utils/axiosWithAuth';
 
 export default function Login(props) {
     // How can we log in? What do we need to do?
 
+ 
+    const [credentials, setCredentials] = useState({
+        username: '',
+        password: ''
+    })
+
+    const handleChanges = e => {
+        setCredentials({
+            ...credentials, [e.target.name]: e.target.value
+            
+        })
+        // console.log('credentials', credentials)
+        
+    }
+
+    const logUserIn = e => {
+        e.preventDefault();
+
+            axiosWithAuth()
+            .post('/login', credentials)
+            .then((res) => {
+                console.log('resonse from login attempt', res)
+                localStorage.setItem('auth-token', res.data.payload)
+                props.history.push('/creatures')
+            })
+
+    }
+ 
+    
+
     return (
         <div>
-            <h1>Welcome to the Safari App!</h1>
-            <h2>I can't show you more until you log in. Please build out a login.</h2>
+            <form onSubmit={logUserIn}>
+                <input
+                    type='text'
+                    name='username'
+                    placeholder='username'
+                    onChange={handleChanges}
+                    value={credentials.username}
+                />
+                <input
+                    type='password'
+                    name='password'
+                    placeholder='Password'
+                    onChange={handleChanges}
+                    value={credentials.password}
+                />
+                <button type='submit' onSubmit={logUserIn}>Log In</button>
+            </form>
         </div>
     )
 }
